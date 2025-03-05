646846eaff881dfe15d763a357b9b40e0d418ffd
freshmea <freshmea@naver.com>
2025-03-05 12:08:08 +0900

    README.md 업데이트 및 increment.c, literal.c 파일 추가; limit.c에서 추가 정보 출력 및 makefile 수정

--- c_example/limit.c
+++ c_example/limit.c
@@ -9,6 +9,9 @@ int main(void)
     printHello();
     printf("char_bit : %d\n", CHAR_BIT);
     printf("char_max : %d\n", CHAR_MAX);
+    printf("INT_MIN : %d \t INT_MAX : %d\n", INT_MIN, INT_MAX);
+    printf("LONG_MIN : %ld \t LONG_MAX : %ld\n", LONG_MIN, LONG_MAX);
+    printf("INT_MAX + 1 : %d\n", INT_MAX + 1);
     return 0;
 }
 
