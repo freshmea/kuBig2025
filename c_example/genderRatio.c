97c06da9aebccee7bf20cc3a4502b587f4ccc20d
freshmea <freshmea@naver.com>
2025-03-05 12:12:48 +0900

    genderRatio.c 파일 추가 및 성비 계산 기능 구현

---
+++ c_example/genderRatio.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int man, woman;
+    double sum, manRate, womanRate;
+
+    // 입력 받는 코드
+
+    // 연산 하는 코드
+
+    printf("남자의 수는 %d명이고 여자의 수는 %d명이다.\n", man, woman);
+    printf("총 수는 %.f명\n남자의 비율은 %.2f%%\n남자의 비율은 %.2f%%\n", sum, manRate, womanRate);
+    return 0;
+}
