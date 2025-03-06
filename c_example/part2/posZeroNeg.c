b18054784061afdda8eb8d17f1417e6e6289d30c
freshmea <freshmea@naver.com>
2025-03-06 16:12:28 +0900

    새로운 기능 추가: 1부터 10까지 출력하는 one2Ten 및 100부터 10000까지 합산하는 one2TenSum 추가; 대문자 및 소문자 판별하는 bigLetter 추가; makefile 업데이트

--- c_example/part2/posZeroNeg.c
+++ c_example/part2/posZeroNeg.c
@@ -6,22 +6,25 @@
 int main(void)
 {
     int num;
-    char *string;
-    printf("숫자를 넣으시오: ");
+    // char *string;
+    // printf("숫자를 넣으시오: ");
     scanf("%d", &num);
-    if (num > 0)
-    {
-        string = "양수";
-    }
-    else if (num < 0)
-    {
-        string = "음수";
-    }
-    else
-    {
-        string = "영";
-    }
+    // if (num > 0)
+    // {
+    //     string = "양수";
+    // }
+    // else if (num < 0)
+    // {
+    //     string = "음수";
+    // }
+    // else
+    // {
+    //     string = "영";
+    // }
 
-    printf("%d 이 숫자는 %s 입니다.\n", num, string);
+    printf("%d 이 숫자는 %s 입니다.\n",
+           num,
+           num > 0 ? "양수" : num < 0 ? "음수"
+                                      : "영");
     return 0;
 }
