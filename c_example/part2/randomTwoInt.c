3a655e034dcf24459dafd1e718b3ab3ef2d3e487
freshmea <freshmea@naver.com>
2025-03-07 10:19:41 +0900

    랜덤 숫자 두 개 출력 기능 추가 및 비교 결과 출력

--- c_example/part2/randomTwoInt.c
+++ c_example/part2/randomTwoInt.c
@@ -6,9 +6,20 @@
 int main(void)
 {
     int a, b;
-    double rand1;
-    srand((unsigned int)time(NULL));
+    double rand1, rand2;
+    srand((unsigned int)time(NULL)); // random 셋
     rand1 = rand() / (double)RAND_MAX * 100;
+    srand((unsigned int)time(NULL) + 10000); // random 셋
+    rand2 = rand() / (double)RAND_MAX * 100;
+
+    a = rand1;
+    b = rand2;
     printf("0 ~ 100 사이의 랜덤한 숫자: %f\n", rand1);
+    printf("0 ~ 100 사이의 랜덤한 숫자: %f\n", rand2);
+    printf("%d > %d : %s\n", a, b, a > b ? "true" : "false");
+    printf("%d < %d : %s\n", a, b, a < b ? "true" : "false");
+    printf("%d == %d : %s\n", a, b, a == b ? "true" : "false");
+    printf("%d != %d : %s\n", a, b, a != b ? "true" : "false");
+
     return 0;
 }
