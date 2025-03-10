bce8a5883b2b6273c9c93f5dca66925d7226f9cc
freshmea <freshmea@naver.com>
2025-03-10 14:17:14 +0900

    함수 추가: endian.c, pointer2.c, functionPointer.c 파일 생성 및 makefile 업데이트; pointerConcept.c 주석 수정

--- c_example/part3/pointerConcept.c
+++ c_example/part3/pointerConcept.c
@@ -6,9 +6,10 @@ int main(void)
     int *pVar;
 
     var_a = 100;
-    pVar = &var_b; // 포인터는 변수의 연결을 해야 한다.
-    *pVar = 200;
+    pVar = &var_b; // 포인터는 변수와 연결을 해야 한다.
+    *pVar = 200;   // 포인터는 반드시 대상을 가리키고 역참조를 해야 한다.
     printf("var_a : %d, &var_a : %p\n", var_a, &var_a);
     printf("*pVar: %d, pVar : %p\n", *pVar, pVar);
+    printf("var_b : %d, &var_b : %p\n", var_b, &var_b);
     return 0;
 }
