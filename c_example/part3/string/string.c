d762f59ff66554928d178c5652f8930541c1ea8f
freshmea <freshmea@naver.com>
2025-03-11 15:52:30 +0900

    문자열 처리 함수 구현: my_strlen, my_strcpy, my_strcmp, my_strcat 추가 및 예제 코드 작성

---
+++ c_example/part3/string/string.c
@@ -0,0 +1,23 @@
+#include "string.h"
+#include <stdio.h>
+
+int main(void)
+{
+    char *str = "wonderful C class";
+    int len = my_strlen(str);
+    printf("len of str : %d", len);
+
+    char str2[40];
+    strcpy(str2, str);
+    printf("%s\n", str2);
+
+    if (strcmp(str2, str) == 0)
+        printf("두 문자열의 내용은 같다.");
+    else
+        printf("두 문자열의 내용은 다르다.");
+
+    my_strcat(str2, "----");
+    my_strcat(str2, str);
+    printf("%s\n", str2);
+    return 0;
+}
