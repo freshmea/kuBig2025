ca551c8a787e177c036b40202622243d6c2350cf
freshmea <freshmea@naver.com>
2025-03-11 11:07:06 +0900

    랜덤 숫자 생성기 구현: myRandom.h, myRandom.c, main.c 및 makefile 추가, 불필요한 a.out 파일 삭제

--- c_example/part3/myRandom/myRandom.c
+++ c_example/part3/myRandom/myRandom.c
@@ -1,23 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+static int seed = 1;
+// extern int seed;
 
-void my_srand(int s);
-int my_rand(void);
-
-int main(void)
-{
-    my_srand(time(NULL));
-    for (int i = 0; i < 10; ++i)
-    {
-        int num = my_rand();
-        printf("%d ,", num);
-    }
-    printf("\n");
-    return 0;
-}
-
-int seed = 1;
 void my_srand(int s)
 {
     seed = s;
