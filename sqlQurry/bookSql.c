920de810830fc74016c52329fec3a4f9e4f722a0
freshmea <freshmea@naver.com>
2025-03-12 14:25:05 +0900

    MySQL 데이터베이스 연결 및 쿼리 실행 기능 추가

--- sqlQurry/bookSql.c
+++ sqlQurry/bookSql.c
@@ -4,8 +4,46 @@
 // sudo apt install libmysql++*
 // dpkg -L libmysqlclient-dev | grep mysql.h
 // cc -o bookSql bookSql.c -I/usr/include/mysql -L/usr/lib/mysql -lmysqlclient
-
+// libmysqlclient.so libmysqlclient.a
 int main(void)
 {
+    MYSQL *conn;
+    MYSQL_RES *res;
+    MYSQL_ROW row;
+    char *host = "localhost";
+    char *user = "myuser";
+    char *pass = "0000";
+    char *db = "mydb";
+    char query[255];
+    int port = 3306;
+
+    strcpy(query, "select * from Book");
+
+    // 연결
+    conn = mysql_init(NULL); // DB 연결 요청 준비
+    if (mysql_real_connect(conn, host, user, pass, db, port, NULL, 0))
+        printf("MySQL 연결 성공\n");
+    else
+    {
+        printf("MySQL 연결 실패\n");
+        return 1;
+    }
+
+    // 쿼리 요청
+    if (mysql_query(conn, query))
+    {
+        printf("쿼리 실패");
+        return 1;
+    }
+    res = mysql_store_result(conn);
+    while (row = mysql_fetch_row(res))
+    {
+        printf("%s\t", row[0]);
+        printf("%s\t", row[1]);
+        printf("%s\t", row[2]);
+        printf("%d\t\n", atoi(row[3]));
+    };
+    mysql_close(conn);
+
     return 0;
 }
