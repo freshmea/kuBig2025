667fdaa895a6bd3a4ea09d4e3a8f09d5e04776b3
freshmea <freshmea@naver.com>
2025-03-14 12:20:26 +0900

    putSeries.c 파일 수정: 파일 열기 모드를 'a'로 변경하고 출력 형식 수정, fflushEx.c 파일 추가

--- c_example/part3/putSeries.c
+++ c_example/part3/putSeries.c
@@ -3,7 +3,7 @@
 int main(void)
 {
     FILE *fp;
-    fp = fopen("output.txt", "w");
+    fp = fopen("output.txt", "a");
     if (fp == NULL)
     {
         printf("파일 열기 실패");
@@ -15,10 +15,10 @@ int main(void)
     fputc('B', stdout);
     // gets deprecate 지원 종료
     fputs("puts로 문자가 간다.", fp);
-    fputs("puts로 문자가 간다.", stdout);
+    fputs("puts로 문자`가 간다.", stdout);
 
-    fprintf("printf로 만자가 간다.", fp);
-    fprintf("printf로 만자가 간다.", stdout);
+    fprintf(fp, "printf로 문자가 간다.");
+    fprintf(stdout, "printf로 문자가 간다.\n");
     // EOF == -1;
     fclose(fp);
     return 0;
