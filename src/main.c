61b2460ab2ea0fa5a649c9c3b6b53420d0f88af5
freshmea <freshmea@naver.com>
2025-03-19 11:54:57 +0900

    코드 정리 및 디바운싱 로직 제거, 숫자 출력 기능 추가

--- src/main.c
+++ src/main.c
@@ -1,65 +1,16 @@
-#include <avr/interrupt.h>
 #include <avr/io.h>
-
-volatile unsigned long milliseconds = 0;
-
-// void calcmilli(void)
-// {
-//     // 계산 하는 코드 ...
-//     ++milliseconds;
-// }
-ISR(TIMER0_OVF_vect)
-{
-    ++milliseconds;
-}
-
-void timer0_init(void)
-{
-    TCCR0 = _BV(CS01) | _BV(CS00);
-    TCNT0 = 6;
-    TIMSK |= _BV(TOIE0);
-    sei();
-}
+#include <util/delay.h>
 
 int main()
 {
-    timer0_init();
-    DDRC = 0x01;  // C 0 비트 출력 설정
-    PORTE = 0xFF; // 풀업 설정
-
-    unsigned long lastDebounceTime = 0;
-    const unsigned long debouceDelay = 20;
-    uint8_t lastButtonState = 1;
-    uint8_t buttonState = 1;
-
+    uint8_t numbers[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x27, 0x7F, 0x67};
+    int count = 0;
+    DDRA = 0xFF;
     while (1)
     {
-        uint8_t reading = PINE & _BV(PE0);
-
-        // 버튼 상태 변경 확인
-        if (reading != lastButtonState)
-        {
-            lastDebounceTime = milliseconds;
-        }
-
-        // 디바운스 시간 경과 확인
-        if ((milliseconds - lastDebounceTime) > debouceDelay)
-        {
-            // 실제 버튼 상태 업데이트
-            if (reading != buttonState)
-            {
-                buttonState = reading;
-
-                // 버튼이 눌렸을 때 동작 수행
-                if (buttonState == 0)
-                {
-                    PORTC ^= _BV(PC0);
-                }
-            }
-        }
-
-        // 마지막 상태 저장
-        lastButtonState = reading;
+        PORTA = numbers[count];
+        count = (count + 1) % 10;
+        _delay_ms(500);
     }
 
     return 0;
