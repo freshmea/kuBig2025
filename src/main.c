10b87a703849b2dba2bac0dd78ee2ab4db100d56
freshmea <freshmea@naver.com>
2025-03-19 16:46:33 +0900

    UART 수신 기능 추가 및 FND 출력 로직 수정

--- src/main.c
+++ src/main.c
@@ -1,39 +1,39 @@
-// 인터럽트로 FND 를 점멸.
-// INT5  phase 1 -> phase 2 or phase 2 -> phase 1
-// phase 1 : FND 가 빠르게 상승하는 코드 ms 100
-// phase 2 : FND 를 순간 멈추게 함.
 #include <avr/interrupt.h>
 #include <avr/io.h>
-#include <util/delay.h>
 
-volatile uint8_t timeStop = 0; // 인터럽트에서 쓸 변수
+volatile uint8_t txFlag = 0;
+volatile char txData = 0;
+
+uint8_t getch(void)
+{
+    uint8_t data;
+    while ((UCSR0A & 0x80) == 0) // 문자 버퍼에 있으면 루프 탈출
+        ;
+    data = UDR0;
+    UCSR0A |= 0x80;
+    return data;
+}
 
 int main()
 {
     uint8_t numbers[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x27, 0x7F, 0x67};
-    uint8_t count = 0;
+    uint8_t rxData;
     DDRA = 0xFF;
 
-    EIMSK = 0x20; // 0b00100000  INT5, INT7
-    // EICRB = 0x08; // 0b11001000인터럽트 5는 하강, 인터럽트7은 상승.
-    EICRB = _BV(ISC50) | _BV(ISC51);
-    sei();
+    UCSR0A = 0x00;
+    UCSR0B = 0x18; // 0b00011000 Rx, Tx enable
+    UCSR0C = 0x16; // 0b00010110  비동기, no Parity, 1 stop bit
+
+    UBRR0H = 0x00;
+    UBRR0L = 0x07; // 115200 bps
 
     while (1)
     {
-        if (timeStop == 0)
+        rxData = getch();
+        if ((rxData >= 0x30) && (rxData <= 0x39))
         {
-            PORTA = numbers[count];
-            count = (count + 1) % 10;
+            PORTA = numbers[rxData - 0x30];
         }
-        _delay_ms(100);
     }
     return 0;
-}
-
-SIGNAL(INT5_vect)
-{
-    cli(); // sei() 반대 설정 SREG -> I unset -> 0
-    timeStop ^= 1;
-    sei();
 }
