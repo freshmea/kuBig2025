41aca8211b7169c81124775be05173de1ada04cc
freshmea <freshmea@naver.com>
2025-03-19 10:37:10 +0900

    불필요한 LCD 관련 코드 제거 및 정리

--- atmega128/ch3_6_chattering_error.c
+++ atmega128/ch3_6_chattering_error.c
@@ -1,8 +1,4 @@
-#include "lcd.h"
 #include <avr/io.h>
-#include <stdio.h>
-#include <string.h>
-#include <util/delay.h>
 
 int main()
 {
@@ -10,9 +6,6 @@ int main()
     DDRC = 0x0F;
     PORTE = 0x01; // 풀업 설정
 
-    lcdInit();
-    lcdClear();
-    char lcdBuffer[17];
     while (1)
     {
         if (PINE & _BV(PE0))
@@ -21,14 +14,6 @@ int main()
         }
         if (!(switchFlag & _BV(PE0)))
             PORTC ^= 0x01; // 1 번 비트 반전
-
-        // PORTC ^= 0x01;
-        // _delay_ms(500);
-        lcdGotoXY(0, 0);
-        lcdBuffer[17] = "";
-        sprintf(lcdBuffer, "%d", switchFlag & _BV(PE0));
-        // strcat(lcdBuffer, '0' + PORTC);
-        lcdPrint(lcdBuffer);
     }
 
     return 0;
