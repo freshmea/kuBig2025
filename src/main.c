4bfe65023d3c8532d8bfcd696312346f49123e58
freshmea <freshmea@naver.com>
2025-03-24 09:48:01 +0900

    main.c에서 PWM 관련 코드 삭제 및 모터 제어 로직 추가

--- src/main.c
+++ src/main.c
@@ -1,40 +1,21 @@
-#include "lcd.h"
 #include <avr/delay.h>
 #include <avr/io.h>
-#include <stdlib.h>
 
 int main(void)
 {
-    DDRE = _BV(PE3); // PE3 출력 설정
-
-    TCCR3A = _BV(COM3A1) | _BV(WGM31);
-    TCCR3B = _BV(WGM33) | _BV(WGM32) | _BV(CS31);
-    // 분주비 8, fast PWM mode, timer3
-    ICR3 = 40000; // 20ms 2MHz -> 2000000 -> 1초 ? -> 0.02초
-    OCR3A = 3000; // 40000 : 20 = ? :1.5  2000~4000 -> -90 ~ 90
-
-    lcdInit();
-    lcdClear();
-    char buffer[16];
+    DDRD = _BV(PD4) | _BV(PD5); // 출력 설정
+    DDRB = _BV(PB5);
 
     while (1)
     {
-        for (uint16_t pulse = 2000; pulse <= 4000; pulse += 40)
-        {
-            OCR3A = pulse;
-            itoa(pulse, buffer, 10);
-            lcdGotoXY(0, 0);
-            lcdPrint(buffer);
-            _delay_ms(20);
-        }
-        for (uint16_t pulse = 4000; pulse >= 2000; pulse -= 40)
-        {
-            OCR3A = pulse;
-            itoa(pulse, buffer, 10);
-            lcdGotoXY(0, 0);
-            lcdPrint(buffer);
-            _delay_ms(20);
-        }
+        PORTD |= _BV(PD4); // M1 정회전
+        PORTB |= _BV(PB5); // M1 회전 enable
+        _delay_ms(500);
+        PORTD &= ~_BV(PD4); // 0bXXX1XXXX -> & (0b 11101111 )
+        PORTD |= _BV(PD5);  // M1 정회전
+        PORTB |= _BV(PB5);  // M1 회전 enable
+        _delay_ms(500);
     }
+
     return 0;
 }
