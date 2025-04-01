0c550c4d81b09c21677ba36791b57c9bb05ddb4a
freshmea <freshmea@gmail.com>
2025-04-01 16:57:20 +0900

    모듈 exam 추가: 커널 모듈 초기화 및 종료 함수 구현, Makefile 추가

---
+++ raspberryPi/module/.module_exam.mod.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/aa/kuBig2025/raspberryPi/module/module_exam.mod := printf '%s\n'   module_exam.o | awk '!x[$$0]++ { print("/home/aa/kuBig2025/raspberryPi/module/"$$0) }' > /home/aa/kuBig2025/raspberryPi/module/module_exam.mod
---
+++ raspberryPi/module/.modules.order.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/aa/kuBig2025/raspberryPi/module/modules.order := {   echo /home/aa/kuBig2025/raspberryPi/module/module_exam.o; :; } > /home/aa/kuBig2025/raspberryPi/module/modules.order
