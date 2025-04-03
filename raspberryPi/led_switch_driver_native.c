e2742217601099679fc9b527ce3c2dc6380c8bda
freshmea <freshmea@gmail.com>
2025-04-03 16:40:51 +0900

    LED 스위치 드라이버 모듈 추가: led_switch_driver.c, Makefile 및 관련 파일 생성 스위치 드라이버 네이티브 테스트 프로그램 수정: 드라이버 열기 모드 변경

--- raspberryPi/led_switch_driver_native.c
+++ raspberryPi/led_switch_driver_native.c
@@ -1,4 +1,4 @@
-//  cc -o switch_driver_native switch_driver_native.c
+//  cc -o led_switch_driver_native led_switch_driver_native.c
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,16 +8,27 @@ int main(void)
 {
     int fd;
     char buf[4] = {0};
-    fd = open("/dev/switch_driver", O_RDWR);
+    fd = open("/dev/led_switch_driver", O_RDWR);
     if (fd < 0)
     {
         printf("driver open fail\n");
         return -1;
     }
-    for (int i = 0; i < 10; ++i)
+
+    for (int i = 0; i < 4; ++i)
     {
         read(fd, &buf, 4);
         printf("sw1: %d, sw2: %d, sw3: %d, sw4: %d\n", buf[0], buf[1], buf[2], buf[3]);
+        sleep(1);
+    }
+    buf[0] = 0;
+    buf[1] = 0;
+    buf[2] = 0;
+    buf[3] = 0;
+    for (int i = 0; i < 4; ++i)
+    {
+        buf[i] = 1;
+        write(fd, &buf, 4);
         sleep(1);
     }
     close(fd);
---
+++ raspberryPi/module/led_switch_driver/led_switch_driver.mod
@@ -0,0 +1,1 @@
+/home/aa/kuBig2025/raspberryPi/module/led_switch_driver/led_switch_driver.o
