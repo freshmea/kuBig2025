e29d7111733914486b781784b33b4e349a49e5fa
freshmea <freshmea@gmail.com>
2025-04-03 17:27:45 +0900

    LED 스위치 드라이버 개선: 스위치 읽기 루프 반복 횟수 조정 및 지연 추가

--- raspberryPi/led_switch_driver_native.c
+++ raspberryPi/led_switch_driver_native.c
@@ -15,10 +15,11 @@ int main(void)
         return -1;
     }
 
-    for (int i = 0; i < 4000; ++i)
+    for (int i = 0; i < 4; ++i)
     {
         read(fd, &buf, 4);
         printf("sw1: %d, sw2: %d, sw3: %d, sw4: %d\n", buf[0], buf[1], buf[2], buf[3]);
+        sleep(1);
     }
     for (int i = 0; i < 4; ++i)
     {
