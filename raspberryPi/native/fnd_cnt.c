fa6890065c55d7f0c0638fa557ddc3184b47ea39
freshmea <freshmea@gmail.com>
2025-04-04 10:47:20 +0900

    fnd_cnt.c에 FND 숫자 테스트 기능 추가 및 makefile에 fnd_cnt 추가

--- raspberryPi/native/fnd_cnt.c
+++ raspberryPi/native/fnd_cnt.c
@@ -12,11 +12,33 @@ void bit_74HC595_shift16(uint8_t digit1, uint8_t digit2);
 
 int main(void)
 {
+    wiringPiSetupGpio();
+    pinMode(DATA_PIN, OUTPUT);
+    pinMode(LATCH_PIN, OUTPUT);
+    pinMode(CLOCK_PIN, OUTPUT);
+
+    digitalWrite(DATA_PIN, LOW);
+    digitalWrite(LATCH_PIN, LOW);
+    digitalWrite(CLOCK_PIN, LOW);
+
+    printf("테스트 FND 숫자 테스트");
+    for (int i = 0; i < 10; ++i)
+    {
+        bit_74HC595_shift16(numbers[i % 10], numbers[0]);
+        delay(300);
+    }
+    for (int i = 0; i < 10; ++i)
+    {
+        bit_74HC595_shift16(numbers[0], numbers[i % 10]);
+        delay(300);
+    }
+    printf("FND 끄기");
+    bit_74HC595_shift16(0x00, 0x00);
 }
 
 void bit_74HC595_shift16(uint8_t digit1, uint8_t digit2)
 {
-    uint16_t data = (digit1 << 9) | (digit2 << 1);
+    uint16_t data = (digit1 << 8) | (digit2 << 0);
 
     for (int i = 15; i >= 0; i--)
     {
