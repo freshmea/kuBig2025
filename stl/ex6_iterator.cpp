2882af86926d0110238c8dfa531aca6d72216355
freshmea <freshmea@naver.com>
2025-04-21 17:16:32 +0900

    ex6_iterator.cpp에서 반복자 선언을 for 루프 내로 이동하여 코드 가독성 향상

--- stl/ex6_iterator.cpp
+++ stl/ex6_iterator.cpp
@@ -8,12 +8,18 @@ using namespace std;
 
 int main()
 {
-    vector<int>::iterator it;
     vector<int> test_vector = {1, 2, 3, 4, 5, 6};
 
-    for (it = test_vector.begin(); it != test_vector.end(); ++it)
+    for (vector<int>::iterator it = test_vector.begin(); it != test_vector.end(); ++it) // for 문 두번째 방식
     {
         cout << *it << endl;
+    }
+
+    // iterator 가 정의 - begin(), end()
+    // *it 역참조가 가능해야 한다.
+    for (auto v : test_vector) // 세번째 방식 모던 C++
+    {
+        cout << v << endl;
     }
 
     return 0;
