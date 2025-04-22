264d36003986509dc9c292a833f4480d6e7b6eb5
freshmea <freshmea@naver.com>
2025-04-22 12:25:27 +0900

    stl/data_process 모듈 추가: DataProcessor 클래스 구현 및 메인 함수 작성

---
+++ stl/data_process/main.cpp
@@ -0,0 +1,16 @@
+#include "data_process.hpp"
+#include <iostream>
+
+int main()
+{
+    std::vector<std::string> words = {"apple", "ant", "banana", "bear", "cat"};
+    DataProcessor processor(words);
+
+    auto sorted = processor.sort_by_length();
+    std::cout << "Sorted by length : " << std::endl;
+    for (const auto &word : processor.data_)
+        std::cout << word << std::endl;
+
+    for (const auto &word : sorted)
+        std::cout << word << std::endl;
+}
