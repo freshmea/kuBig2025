ba161cf45666c344de4ab738aca9ca8282580bd6
freshmea <freshmea@naver.com>
2025-04-22 14:21:56 +0900

    main.cpp에 mutable 캡처를 사용하는 count_long 함수 추가 및 관련 출력 수정

--- stl/data_process/main.cpp
+++ stl/data_process/main.cpp
@@ -20,5 +20,23 @@ int main()
     for (const auto &word : filtered)
         cout << word << endl;
 
+    // capture mutable 사용
+    int min_length = 4;
+    auto count_long = [=]() mutable
+    {
+        int count = 0;
+        for (const auto &word : words)
+        {
+            if (word.length() >= min_length)
+            {
+                count++;
+            }
+        }
+        min_length++;
+        cout << "min_length" << min_length << endl;
+        return count;
+    };
+    cout << "Words with length >= 4" << count_long() << endl;
+    cout << "After mutable change, min_length: " << min_length << endl;
     return 0;
 }
