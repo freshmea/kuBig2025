5c08f75ba89a7236d2b45e948f2d20d2457131bc
freshmea <freshmea@naver.com>
2025-04-24 14:10:08 +0900

    log.txt 파일에 이름 및 숫자 프린트 예제 추가 ex11_moveClass.cpp 파일에서 MyData 클래스의 소유권 관리 및 이동 생성자/대입 연산자 구현 개선 ex14_anyncFuture.cpp 파일에서 비동기 계산 예제 수정 및 결과 출력 방식 변경

--- smartpoint/ex14_anyncFuture.cpp
+++ smartpoint/ex14_anyncFuture.cpp
@@ -1,18 +1,26 @@
 #include <chrono>
 #include <future>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int slowAdd(int a, int b)
 {
-    this_thread::sleep_for(chrono::seconds(2));
+    for (int i = 0; i < 5; ++i)
+    {
+        cout << "slowAdd 실행 중 " << a << endl;
+        this_thread::sleep_for(chrono::microseconds(400'000));
+    }
     return a + b;
 }
 
 int main()
 {
-    future<int> result = async(slowAdd, 2, 3);
+    vector<future<int>> result;
+    for (int i = 0; i < 4; ++i)
+        result.push_back(async(slowAdd, i + 1, 3));
     cout << "계산 중 ..." << endl;
-    cout << "결과 : " << result.get() << endl;
+    for (int i = 0; i < 4; ++i)
+        cout << "결과 : " << result[i].get() << endl;
     return 0;
 }
