6a4c470b928b121ca738db7b3c9ec7b060187a64
freshmea <freshmea@naver.com>
2025-04-29 16:42:51 +0900

    서버 코드 수정: 클라이언트의 IP와 포트 정보를 출력하는 기능 추가

--- network/tcp/uecho_server.c
+++ network/tcp/uecho_server.c
@@ -44,6 +44,7 @@ int main(int argc, char *argv[])
         char message[BUF_SIZE];
         str_len = recvfrom(serv_sock, message, BUF_SIZE, 0, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
         message[str_len] = '\0';
+        printf("클라이언트의 IP : PORT : %s, %d\n", inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
         printf("들어온 메세지는 : %s \n", message);
         sendto(serv_sock, message, str_len, 0, (struct sockaddr *)&clnt_addr, sizeof(clnt_addr));
     }
