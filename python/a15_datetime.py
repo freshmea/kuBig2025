330338d1b2ff0bc27b310b2dab1f038483ec1a18
freshmea <freshmea@naver.com>
2025-05-08 11:22:14 +0900

    코드 정리: 불필요한 공백 제거 및 새로운 파일 a16_datatime2.py 생성

--- python/a15_datetime.py
+++ python/a15_datetime.py
@@ -15,10 +15,10 @@ class Myclass:
 def main():
     ptime = datetime.datetime.now()
     print(ptime) # __str__ 오버로딩이 되어서 나온다.
-    
+
     myclass =Myclass()
     print(myclass)
-    
+
     print(ptime.month)
     print(f"{ptime.year} 년 {ptime.month} 월 {ptime.day} 일")
 
---
+++ python/a16_datatime2.py
@@ -0,0 +1,9 @@
+# if elif else 사용해서 현재 시각 오전 오후 표시
+# 계절을 표시 하는 프로그램
+# datetime 모듈 활용
+# input 받아서 나타나게(시간,월) -> "now"-> now() 함수를 불러와서 표시.
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
