c4d85c8357460ba9518b9c1b661db7fbeed90c98
freshmea <freshmea@naver.com>
2025-05-09 09:35:06 +0900

    주석 추가: a21_cPython_import.py에 설치 및 실행 관련 주석 추가

--- python/a21_cPython_import.py
+++ python/a21_cPython_import.py
@@ -1,3 +1,8 @@
+# python3 setup.py build
+# sudo python3 setup.py install
+# (option) 지우는 코드 sudo pip uninstall mymodule
+# 실행
+# (option) 개발 폴더에 .so 파일 옮기기 -> /root 가 아닌곳에서도 실행 됨
 import mymodule
 
 # from mymodule import Hello
