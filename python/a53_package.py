1eb3a867b5bcd2d0e2963c619db4560d2092c60b
freshmea <freshmea@naver.com>
2025-05-12 15:45:55 +0900

    함수 호출 방식 수정: module_a_func()의 출력 제거

--- python/a53_package.py
+++ python/a53_package.py
@@ -6,7 +6,7 @@ from test_package.module_a import module_var_a
 
 def main():
     print(test_package.module_a.module_var_a)
-    print(test_package.module_a.module_a_func())
+    test_package.module_a.module_a_func()
     print(test_package.module_b.module_var_b)
     print(test_package.module_b.Module_B())
     print(module_var_a)
