c0b8e81d13397a016e081d29dd518a8610c9d28b
freshmea <freshmea@naver.com>
2025-05-13 17:24:44 +0900

    Flask 애플리케이션에서 Redis 캐시 기능 제거 및 기본 응답 수정, 새로운 학생 생성 기능 추가

--- docker_test/app/app2.py
+++ docker_test/app/app2.py
@@ -18,12 +18,28 @@
 # numpy, pandas, matplotlib, tensorflow( keras ), pytorch, sk-learn(머신러닝)
 # orange3
 
+import time
 
+import redis
 from flask import Flask
 
 app = Flask(__name__)
+cache = redis.Redis(host="redis", port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr("hits")
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 
 @app.route("/")
 def hello():
-    return "<h1>Hello Word! I'm python Flask!!</h1>"
+    count = get_hit_count()
+    return f"<h1>Hello Word! I'm python Flask!!{count}.</h1>"
