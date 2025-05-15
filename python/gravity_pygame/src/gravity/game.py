186875b7f1b8b4577e318cd986d4cd3260743612
freshmea <freshmea@naver.com>
2025-05-15 23:00:57 +0900

    UI 클래스 분리 및 모듈화

--- python/gravity_pygame/src/gravity/game.py
+++ python/gravity_pygame/src/gravity/game.py
@@ -1,27 +1,7 @@
-import time
-
 import pygame
 
 from .ball import Ball
-
-
-class UI(pygame.sprite.Sprite):
-    def __init__(self, game):
-        super().__init__()
-        self.game = game
-        self.font = pygame.font.SysFont(None, 50)
-        self.image = None
-        self.rect = None
-        self.start_time = time.time()
-
-    def update(self):
-        elapsed_time = time.time() - self.start_time
-        ui_text = f"Elapsed Time : {elapsed_time:.2f}, ball Count: {len(self.game.all_sprite)}"
-        self.image = self.font.render(ui_text, True, (0, 0, 0))
-        self.rect = self.image.get_rect(topleft=(10, 10))
-
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
+from .ui import UI
 
 
 class Game:
