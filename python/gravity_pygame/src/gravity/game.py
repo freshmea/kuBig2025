afac96ef4bd81bc7a8a02df2ba0d2c626b16e51a
freshmea <freshmea@naver.com>
2025-05-15 12:28:55 +0900

    aa

---
+++ python/gravity_pygame/src/gravity/game.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 600))
+        self.game_active = True
+
+    def run(self):
+        while True:
+            if self.game_active:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        exit()
+
+            else:
+                break
