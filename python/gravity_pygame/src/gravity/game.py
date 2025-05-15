94ae0c5d1287b15d66e1f26a4a647ba18eb68d96
freshmea <freshmea@naver.com>
2025-05-15 15:02:41 +0900

    Ball 클래스를 pygame.sprite.Sprite로 상속받도록 수정하여 스프라이트 기능 통합 및 업데이트 로직 개선

--- python/gravity_pygame/src/gravity/game.py
+++ python/gravity_pygame/src/gravity/game.py
@@ -5,29 +5,34 @@ import pygame
 Vec = pygame.math.Vector2
 
 
-class Ball:
+class Ball(pygame.sprite.Sprite):
     def __init__(self):
-        self.x = random.randint(0, 800)
-        self.y = random.randint(0, 600)
-        self.radius = random.uniform(50, 100)
+        super().__init__()
+        self.size = random.randint(10, 30)
+        self.image = pygame.Surface((self.size * 2, self.size * 2))
         self.color = (
             random.randint(0, 255),
             random.randint(0, 255),
             random.randint(0, 255),
         )
+        pygame.draw.circle(self.image, self.color, (self.size, self.size), self.size)
+        self.image.set_colorkey((0, 0, 0))
+        self.x = random.randint(0, 800)
+        self.y = random.randint(0, 600)
+        self.rect = self.image.get_rect()
+        self.rect.center = Vec(self.x, self.y)  # type: ignore
         self.vel = Vec(random.randint(-10, 10), random.randint(-10, 10))
 
     def update(self):
-        self.x += self.vel.x
-        self.y += self.vel.y
-        if self.x < 0:
-            self.x = 800
-        if self.x > 800:
-            self.x = 0
-        if self.y < 0:
-            self.y = 600
-        if self.y > 600:
-            self.y = 0
+        self.rect.center += self.vel  # type: ignore
+        if self.rect.x < 0:
+            self.rect.x = 800
+        if self.rect.x > 800:
+            self.rect.x = 0
+        if self.rect.y < 0:
+            self.rect.y = 600
+        if self.rect.y > 600:
+            self.rect.y = 0
 
 
 class Game:
@@ -37,16 +42,17 @@ class Game:
         self.game_active = True
         self.clock = pygame.time.Clock()
         self.fps = 60
-        self.balls = [Ball() for _ in range(5)]
+        self.all_sprite = pygame.sprite.Group()
 
     def update(self):
-        for ball in self.balls:
-            ball.update()
+        if len(self.all_sprite) < 15:
+            ball = Ball()
+            self.all_sprite.add(ball)
+        self.all_sprite.update()
 
-    def update_screen(self):
+    def draw(self):
         self.screen.fill((255, 255, 255))
-        for ball in self.balls:
-            pygame.draw.circle(self.screen, ball.color, (ball.x, ball.y), ball.radius)
+        self.all_sprite.draw(self.screen)
         pygame.display.flip()
 
     def run(self):
@@ -58,6 +64,6 @@ class Game:
                         pygame.quit()
                         exit()
                 self.update()  # 게임 로직에 필요한 내부 변수...
-                self.update_screen()  # 화면 출력을 담당.
+                self.draw()  # 화면 출력을 담당.
             else:
                 break
