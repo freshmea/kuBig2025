2c0a8b15761b5bca44ff9d89d6a814bf8cddaa69
freshmea <freshmea@naver.com>
2025-05-27 11:18:40 +0900

    이미지 불러오기 실패 시 오류 메시지 추가 및 창 크기 조정

--- opencv/part1/01_helloWorld.cpp
+++ opencv/part1/01_helloWorld.cpp
@@ -11,8 +11,16 @@ int main()
     cout << "hello, world" << endl;
 
     Mat img = imread(folderPath + "lenna.bmp");
+    if (img.empty())
+    {
+        cerr << "이미지 불러오기 실패!! ";
+        return -1;
+    }
 
     // cout << img << endl;
+    namedWindow("lenna", WINDOW_AUTOSIZE);
+    Size size(640, 480);
+    resizeWindow("lenna", size);
 
     imshow("lenna", img);
     waitKey();
