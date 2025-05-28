70be2da9168775258bd9b90b9c17e9cab6e7b279
freshmea <freshmea@naver.com>
2025-05-28 14:22:09 +0900

    키보드 입력 처리 개선: waitKeyEx()를 waitKey()로 변경 및 마우스 이벤트 처리 기능 추가

--- opencv/part2/14_mouse.cpp
+++ opencv/part2/14_mouse.cpp
@@ -7,44 +7,37 @@ using namespace cv;
 
 String folderPath = "/home/aa/kuBig2025/opencv/data/";
 
+void on_mouse(int event, int x, int y, int flags, void *);
+
 int main()
 {
     Mat img = imread(folderPath + "lenna.bmp");
     namedWindow("img");
+    setMouseCallback("img", on_mouse);
     auto i = getTickCount();
     int keycode;
     while (true)
     {
-        keycode = waitKey(0);
         cout << "keycode: " << keycode << endl;
         cout << "i: " << i << endl;
         cout << "fps: " << getTickFrequency() / (getTickCount() - i) << endl;
-
-        if (keycode == 'v' || keycode == 'V')
-        {
-            img = ~img;
-        }
-        if (keycode == 'a' || keycode == 'A')
-        {
-            img = img + 1;
-        }
-        if (keycode == 'd' || keycode == 'D')
-        {
-            img = img - 1;
-        }
-        if (keycode == 65364)
-        {
-            cout << "key up arrow!" << endl;
-        }
-        if (keycode == 65362)
-        {
-            cout << "key down arrow!" << endl;
-        }
-        if (keycode == 27)
-            break;
         i = getTickCount();
         imshow("img", img);
+        keycode = waitKey(33);
     }
     destroyAllWindows();
     return 0;
+}
+
+void on_mouse(int event, int x, int y, int flags, void *)
+{
+    switch (event)
+    {
+    case EVENT_LBUTTONDOWN:
+        cout << "마우스 왼쪽 버튼이 눌렸다!" << endl;
+        break;
+    default:
+        cout << "마우스 이벤트!!" << endl;
+        break;
+    }
 }
