abde3472ac8394da37073b846cf1adefcf4c2bba
freshmea <freshmea@naver.com>
2025-05-28 16:54:41 +0900

    새 파일 추가: 마우스 이벤트를 통해 사각형을 그리는 기능을 구현한 19_draw_rect_mouse.cpp 파일 생성

--- opencv/part2/19_draw_rect_mouse.cpp
+++ opencv/part2/19_draw_rect_mouse.cpp
@@ -12,6 +12,8 @@ using namespace cv;
 
 String folderPath = "/home/aa/kuBig2025/opencv/data/";
 
+void on_mouse(int event, int x, int y, int flags, void *data);
+
 int main()
 {
     VideoCapture cap(0);
@@ -23,14 +25,19 @@ int main()
     cout << "Frame height: " << cvRound(cap.get(CAP_PROP_FRAME_HEIGHT)) << endl;
     auto fps = cap.get(CAP_PROP_FPS);
     cout << "fps : " << cvRound(fps) << endl;
-
+    namedWindow("img");
     Mat frame;
+    Rect rec;
+    setMouseCallback("img", on_mouse, (void *)&rec);
+
     while (true)
     {
         cap >> frame;
         if (frame.empty())
             break; // 마지막 프레임 처리
-        imshow("frame", frame);
+        rectangle(frame, rec, red, 3, LINE_AA);
+        cout << rec << endl;
+        imshow("img", frame);
         if (waitKey(1000 / fps) == 27) // fps 조절 숫자.
             break;
     }
@@ -39,4 +46,30 @@ int main()
     destroyAllWindows();
 
     return 0;
+}
+
+void on_mouse(int event, int x, int y, int flags, void *data)
+{
+    Rect *rect = (Rect *)data;
+    static Point temp;
+    switch (event)
+    {
+    case EVENT_LBUTTONDOWN:
+        temp.x = x;
+        temp.y = y;
+        cout << "button down" << endl;
+        break;
+    case EVENT_LBUTTONUP:
+        rect->x = temp.x;
+        rect->y = temp.y;
+        rect->width = x - temp.x;
+        rect->height = y - temp.y;
+        cout << "button up" << endl;
+        break;
+    case EVENT_MOUSEMOVE:
+        break;
+    default:
+        // cout << "마우스 이벤트!!" << endl;
+        break;
+    }
 }
