50a5e71c71823094eafcaa44a367e667a22bf756
freshmea <freshmea@naver.com>
2025-05-29 15:47:43 +0900

    기능 추가: 중간 필터링을 적용한 28_median.cpp 파일 생성

--- opencv/part3/28_median.cpp
+++ opencv/part3/28_median.cpp
@@ -15,15 +15,22 @@ int main()
     Mat img = imread(folderPath + "lenna.bmp", IMREAD_GRAYSCALE);
     vector<Mat> images;
     images.push_back(img.clone());
-    Mat noise(img.size(), CV_32SC1);
-    randn(noise, 0, 15);
-    add(img, noise, img, Mat(), CV_8U);
+
+    int num = (int)(img.total() * 0.1);
+    for (int i = 0; i < num; ++i)
+    {
+        int x = rand() % img.cols;
+        int y = rand() % img.rows;
+        img.at<uchar>(y, x) = (i % 2) * 255;
+    }
+
     images.push_back(img);
-    images.emplace_back(Mat()); // 벡터 내부에 바로 생성.
-    images.emplace_back(Mat());
+    for (int i = 0; i < 3; ++i)
+        images.emplace_back(Mat()); // 벡터 내부에 바로 생성.
+
     GaussianBlur(img, images[2], Size(), 5);
     bilateralFilter(img, images[3], -1, 10, 5);
-
+    medianBlur(img, images[4], 5);
     int i = 1;
     for (auto img : images)
     {
