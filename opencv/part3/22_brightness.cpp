52976c856880f66fcd0822bb0ea0365a3e185e48
freshmea <freshmea@naver.com>
2025-05-29 11:39:56 +0900

    기능 추가: 이미지 밝기 및 대비 조정 기능을 개선하고 히스토그램 생성 기능을 추가한 23_histogram.cpp 파일 생성

--- opencv/part3/22_brightness.cpp
+++ opencv/part3/22_brightness.cpp
@@ -11,16 +11,26 @@ String folderPath = "/home/aa/kuBig2025/opencv/data/";
 
 int main()
 {
-    Mat img = imread(folderPath + "lenna.bmp", IMREAD_GRAYSCALE);
+    Mat img = imread(folderPath + "hawkes.bmp", IMREAD_GRAYSCALE);
     namedWindow("img");
     Mat img2 = img + 100; // bright
     Mat img3 = 2.f * img; // contrast 대조
     Mat img4 = img + (img - 128) * 1.f;
 
+    double min, max;
+    minMaxLoc(img, &min, &max);
+
+    Mat img5 = (img - min) * 255 / (max - min); // stretching
+
+    Mat img6;
+    equalizeHist(img, img6); // 히스토 평활화!
+
     imshow("img", img);
     imshow("img2", img2);
     imshow("img3", img3);
     imshow("img4", img4);
+    imshow("img5", img5);
+    imshow("img6", img6);
     waitKey();
     destroyAllWindows();
     return 0;
