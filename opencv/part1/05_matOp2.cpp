f2f621d8042e6efe00a7ca83af4d612ae255ae19
freshmea <freshmea@naver.com>
2025-05-30 14:32:17 +0900

    기능 변경: CMakeLists.txt에서 opencv/part1 서브디렉토리 추가 및 05_matOp2.cpp에서 주석 처리된 코드 수정

--- opencv/part1/05_matOp2.cpp
+++ opencv/part1/05_matOp2.cpp
@@ -21,7 +21,11 @@ int main()
     {
         for (int j = 0; j < 10; ++j)
         {
-            img.at<uint8_t>(220 + i, 120 + j) = (255, 255, 255);
+            // img.at<uint8_t>(220 + i, 120 + j) = (255, 255, 255);
+            auto &pixel = img.at<Vec3b>(120 + i, 220 + j);
+            pixel[0] = 255;
+            pixel[1] = 255;
+            pixel[2] = 255;
         }
     }
     cout << img3.dims << img3.rows << img3.cols << img3.size() << img3.type() << img3.depth() << endl;
