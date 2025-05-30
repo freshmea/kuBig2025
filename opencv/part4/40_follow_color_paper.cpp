bdea5e224daad1efd44adef7c35cbed29e436733
freshmea <freshmea@naver.com>
2025-05-30 17:15:42 +0900

    새 파일 추가: 색종이 추적 기능을 위한 40_follow_color_paper.cpp 파일 생성

---
+++ opencv/part4/40_follow_color_paper.cpp
@@ -0,0 +1,9 @@
+// 카메라 켜서 frame 확보
+// 색 공간 -> hsv 로 변경 clone() 해서 따로 처리.
+// 양방향 필터 처리
+// inrange -> 색종이를 인식
+// morph 함수 -> 이진화 Mat 정리
+// labeling
+// 화면에 rectangle 로 표시
+// putText, ft2 로 색종이 or color-paper
+// 사각형과 폰트는 색종이를 따라다녀야 함!
