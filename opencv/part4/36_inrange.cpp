4fecd1c5edfbd16c32f90f574046b9db523a2772
freshmea <freshmea@naver.com>
2025-05-30 15:28:53 +0900

    기능 변경: 이미지 필터링을 위한 색상 범위 조정 및 초기화 코드 수정

--- opencv/part4/36_inrange.cpp
+++ opencv/part4/36_inrange.cpp
@@ -19,13 +19,13 @@ int main()
 {
     Mat img = imread(folderPath + "candies.png");
     Mydata mydata;
+    mydata.images.push_back(img.clone());
     cvtColor(img, img, COLOR_BGR2HSV);
-    mydata.images.push_back(img);
     mydata.images.emplace_back(Mat());
     namedWindow("img2");
     createTrackbar("lower Hue", "img2", &mydata.lower_hue, 179, on_hue_changed, (void *)&mydata);
     createTrackbar("upper Hue", "img2", &mydata.upper_hue, 179, on_hue_changed, (void *)&mydata);
-    on_hue_changed(0, 0);
+    // on_hue_changed(0, 0);
     // vector img show 코드
     int i = 1;
     for (auto img : mydata.images)
@@ -42,7 +42,7 @@ int main()
 void on_hue_changed(int pos, void *data)
 {
     Mydata *mydata = (Mydata *)data;
-    Scalar lowerb(mydata->lower_hue, 100, 0);
+    Scalar lowerb(mydata->lower_hue, 0, 0);
     Scalar upperb(mydata->upper_hue, 255, 255);
     inRange(mydata->images[0], lowerb, upperb, mydata->images[1]);
     imshow("img2", mydata->images[1]);
