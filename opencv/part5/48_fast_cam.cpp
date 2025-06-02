5fc779909ae200ab2698fdf8fb1c6665d9fae184
freshmea <freshmea@naver.com>
2025-06-02 14:40:55 +0900

    기능 개선: ORB 특징 추출기를 사용하여 이미지에서 키포인트를 감지하고 그리기

--- opencv/part5/48_fast_cam.cpp
+++ opencv/part5/48_fast_cam.cpp
@@ -11,17 +11,29 @@ String folderPath = "/home/aa/kuBig2025/opencv/data/";
 int main()
 {
     VideoCapture cap(0);
+    Ptr<Feature2D> feature = ORB::create();
     vector<KeyPoint> keypoints;
-    Mat img, gray_img;
+    // vector<Point2f> corners;
+    Mat img, gray_img, desc;
     while (true)
     {
         cap >> img;
         cvtColor(img, gray_img, COLOR_BGR2GRAY);
-        FAST(gray_img, keypoints, 60, true);
-        for (auto &k : keypoints)
-        {
-            circle(img, Point(cvRound(k.pt.x), cvRound(k.pt.y)), 5, red, 2);
-        }
+        // FAST(gray_img, keypoints, 60, true);
+        // goodFeaturesToTrack(gray_img, corners, 1000, 0.01, 10);
+        // feature->detect(img, keypoints);
+        // feature->compute(img, keypoints, dst);
+        feature->detectAndCompute(img, noArray(), keypoints, desc);
+        drawKeypoints(img, keypoints, img, Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+        // for (auto &k : keypoints)
+        // {
+        //     circle(img, Point(cvRound(k.pt.x), cvRound(k.pt.y)), 5, red, 2);
+        // }
+        // for (auto &p : corners)
+        // {
+        //     circle(img, Point(p.x, p.y), 5, red, 2);
+        // }
 
         imshow("img", img);
         if (waitKey(33) == 27)
