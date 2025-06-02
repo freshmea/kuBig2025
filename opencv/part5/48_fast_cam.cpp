d86baaa3d31a83706a8935b41c830ad7f2a98436
freshmea <freshmea@naver.com>
2025-06-02 14:49:51 +0900

    기능 개선: FAST 및 goodFeaturesToTrack 기능을 활성화하고 이미지 클론 처리 개선

--- opencv/part5/48_fast_cam.cpp
+++ opencv/part5/48_fast_cam.cpp
@@ -13,29 +13,38 @@ int main()
     VideoCapture cap(0);
     Ptr<Feature2D> feature = ORB::create();
     vector<KeyPoint> keypoints;
-    // vector<Point2f> corners;
-    Mat img, gray_img, desc;
+    vector<Point2f> corners;
+    Mat img, img1, img2, gray_img, desc;
     while (true)
     {
         cap >> img;
+        img1 = img.clone();
+        img2 = img.clone();
+
+        // FAST
         cvtColor(img, gray_img, COLOR_BGR2GRAY);
-        // FAST(gray_img, keypoints, 60, true);
-        // goodFeaturesToTrack(gray_img, corners, 1000, 0.01, 10);
+        FAST(gray_img, keypoints, 60, true);
+        for (auto &k : keypoints)
+        {
+            circle(img, Point(cvRound(k.pt.x), cvRound(k.pt.y)), 5, red, 2);
+        }
+        imshow("img", img);
+
+        // goodFeature
+        goodFeaturesToTrack(gray_img, corners, 1000, 0.01, 10);
+        for (auto &p : corners)
+        {
+            circle(img1, Point(p.x, p.y), 5, red, 2);
+        }
+        imshow("img1", img1);
+
+        // ORB
         // feature->detect(img, keypoints);
         // feature->compute(img, keypoints, dst);
-        feature->detectAndCompute(img, noArray(), keypoints, desc);
-        drawKeypoints(img, keypoints, img, Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        feature->detectAndCompute(img2, noArray(), keypoints, desc);
+        drawKeypoints(img2, keypoints, img2, Scalar::all(-1), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
-        // for (auto &k : keypoints)
-        // {
-        //     circle(img, Point(cvRound(k.pt.x), cvRound(k.pt.y)), 5, red, 2);
-        // }
-        // for (auto &p : corners)
-        // {
-        //     circle(img, Point(p.x, p.y), 5, red, 2);
-        // }
-
-        imshow("img", img);
+        imshow("img2", img2);
         if (waitKey(33) == 27)
             break;
     }
