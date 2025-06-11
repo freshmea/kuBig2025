31c29e57695a7062f3c6f0685a0ff61452cb28c5
freshmea <freshmea@naver.com>
2025-06-11 16:09:57 +0900

    Add k-Means workflow and initial data processing script
    - Created a new Orange workflow file for k-Means clustering, including nodes for CSV file import, data preprocessing, PCA, and visualization.
    - Implemented a Python script to process input data using pandas, dropping unnecessary columns and converting boolean values for compatibility with Orange data structures.

---
+++ orange33/corelation.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+data = pd.read_csv("/home/aa/kuBig2025/orange33/basket_analysis.csv")
+corr = data.corr()
+print(corr)
+
+sns.heatmap(corr, annot=True, cmap="coolwarm", vmin=-1, vmax=1)
+plt.show()
