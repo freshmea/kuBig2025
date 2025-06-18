e3521649b58993fd1dd8691b6d9b1341c031213c
freshmea <freshmea@naver.com>
2025-06-18 10:17:05 +0900

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- opencv/part7/60_google_vision.cpp
+++ opencv/part7/60_google_vision.cpp
@@ -1,8 +1,17 @@
 #include "opencv2/opencv.hpp"
 #include <fstream>
 #include <iostream>
+#include <vector>
+
+using namespace cv;
+using namespace cv::dnn;
+using namespace std;
+
+String folderPath = "/home/aa/kuBig2025/opencv/part7";
 
 int main()
 {
+    vector<String> files;
+
     return 0;
 }
