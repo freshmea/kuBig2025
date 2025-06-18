7aefe059a2a41ce21dae2b2d8959d3b1ee30dcb7
freshmea <freshmea@naver.com>
2025-06-18 10:22:56 +0900

    Google Vision 코드 수정 및 분류 클래스 파일 추가

--- opencv/part7/60_google_vision.cpp
+++ opencv/part7/60_google_vision.cpp
@@ -7,11 +7,24 @@ using namespace cv;
 using namespace cv::dnn;
 using namespace std;
 
-String folderPath = "/home/aa/kuBig2025/opencv/part7";
+String folderPath = "/home/aa/kuBig2025/opencv/part7/";
 
 int main()
 {
     vector<String> files;
+    files.push_back("beagle.jpg");
+    files.push_back("cup.jpg");
+    files.push_back("pineapple.jpg");
+    files.push_back("scooter.jpg");
+    files.push_back("space_shuttle.jpg");
+
+    vector<Mat> imgs;
+    for (auto &f : files)
+        imgs.push_back(imread(folderPath + f));
+
+    Net net = readNet(
+        "/home/aa/kuBig2025/opencv/part7/bvlc_googlenet.caffemodel",
+        "/home/aa/kuBig2025/opencv/part7/deploy.prototxt");
 
     return 0;
 }
