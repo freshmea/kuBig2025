edab89cce8e5ae3238b4d6480fe33b423fa3ed26
freshmea <freshmea@gmail.com>
2026-03-20 10:03:08 +0900

    cmake

--- python/basic/a00_default.py
+++ python/basic/a00_default.py
@@ -1,5 +1,11 @@
+class A:
+    def __init__(self) -> None:
+        self.__a = 1
+
 def main():
-    pass
+    a = A()
+    print(a.__a)  # Accessing the protected member _a directly
+    # Note: In Python, this is allowed, but it's a convention to indicate that it should not be accessed directly.
 
 
 if __name__ == "__main__":
